691bd2a6c63647f3cf8634f722d34847e3ca775c
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2022-12-17 03:14:40 -0500

    fixed bug in 1_queries

--- 1_queries/04_all_my_reservations.sql
+++ 1_queries/04_all_my_reservations.sql
@@ -1,9 +1,7 @@
-SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date,average_rating
+SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating) as average_rating
 FROM reservations
 JOIN properties on properties.id = reservations.property_id
-JOIN (SELECT AVG(property_reviews.rating) as average_rating
-FROM properties
-JOIN property_reviews on properties.id = property_id
-GROUP BY properties.id) on property_id.average_rating = reservations.property_id
-WHERE reservations.guest_id = 1
-ORDER BY start_date;
+JOIN property_reviews on properties.id = property_reviews.property_id
+WHERE reservations.guest_id = 4
+GROUP BY reservations.id, properties.title, properties.cost_per_night
+ORDER BY start_date
